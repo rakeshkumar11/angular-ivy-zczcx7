b5ff75b1c845a8850a01dc98a8e0c63f8426d95a
rakeshkumar11 <rakeshxaxo@gmail.com>
2020-06-20 14:35:11 +0530

    4 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  
+ title = 'Angular 7 Project!'; 
+
+  todaydate = new Date(); 
+
+  jsonval = {name:'Rox', age:'25', address:{a1:'Mumbai', a2:'Karnataka'}}; 
+  
+  months = ["Jan", "Feb", "Mar", "April", "May", "Jun", "July", "Aug", 
+      "Sept", "Oct", "Nov", "Dec"]; 
 }
